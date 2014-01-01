2c310c444671215c1696ee9ea52dd1848f4ad7cc
master <wwtt1984@163.com>
2014-01-01 11:11:18 +0800

    first app

---
+++ www/install.js
@@ -0,0 +1,10 @@
+﻿var Install = function () {};
+/**
+ * 设置提示值
+ * @param src
+ * @returns {*}
+ */
+Install.prototype.InstallApk = function (src) {
+    return cordova.exec(null, null,"InstallPlugin","Install",[src]);
+};
+module.exports = (new Install());
