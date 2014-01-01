7e6e250bd55dfe27259123ce40e442f8bc6beb72
master <wwtt1984@163.com>
2014-01-01 11:42:27 +0800

    second

--- src/android/InstallPlugin.java
+++ src/android/InstallPlugin.java
@@ -1,30 +1,37 @@
-package com.toast.toast;
+package com.install.install;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.util.Log;
-import android.widget.Toast;
 
-public class ToastPlugin extends CordovaPlugin {
-    public static String TOAST = "Install";
+import java.io.File;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+
+public class InstallPlugin extends CordovaPlugin {
+    public static String ACTION = "Install";
+
     public boolean execute(String action, JSONArray data,
             CallbackContext callbackContext) throws JSONException {
-        if (TOAST.equals(action)) {
-            Log.i(TOAST, "message:"+data.getString(0)+",length:"+data.getInt(1));
-            toast(data.getString(0), data.getInt(1), callbackContext);
+        if (ACTION.equals(action)) {
+            installAPK(data.getString(0),  callbackContext);
         }
         return false;
     }
-    public synchronized void toast(final String message, final int length,
-            CallbackContext callbackContext) {
+
+    public synchronized void installAPK(final String str,CallbackContext callbackContext) {
         final CordovaInterface cordova = this.cordova;
         Runnable runnable = new Runnable() {
             public void run() {
-                Toast.makeText(cordova.getActivity(), message,length).show();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.fromFile(new File(str)), "application/vnd.android.package-archive");
+                cordova.getActivity().startActivity(intent); //DO NOT forget
             }
         };
-       this.cordova.getActivity().runOnUiThread(runnable);
+         this.cordova.getActivity().runOnUiThread(runnable);
     }
 }
